9d08dfe12742323e6cfe4d0f584f79e387b3370f
Odwa  Nodada <Odwanodada22@gmail.com>
2020-03-17 10:23:04 +0200

    Op exe

---
+++ datatypes/Datatypes.py
@@ -0,0 +1,25 @@
+strSentence = "Lifechoices Coding Academy"
+print(type(strSentence))
+print(strSentence[2])
+
+Number1 = 25
+Number2 = 50.5
+Number3 = 3e+7j
+print(type(Number1),type(Number2),type(Number3))
+
+myList = [65,25,125,78,120,33]
+print(type(myList))
+myList.sort()
+print(myList)
+
+myList.insert(2,37)
+print(myList)
+
+myList.reverse()
+print(myList)
+
+Cars = ("Datsun","Ford","BMW","VW")
+print(type(Cars))
+
+xdict = {"1": "Amy","2": "Tania","3": "Nathem"}
+print(type(xdict))
