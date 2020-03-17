11329d35992e1d52d7f52822cf3e872b95f90b18
Odwa  Nodada <Odwanodada22@gmail.com>
2020-03-17 12:54:33 +0200

    loop2

---
+++ Loops/Loops exe.py
@@ -0,0 +1,22 @@
+#initialising the number
+Number = 1
+#this is the while loop for processing
+while Number < 11:
+    print(Number)
+    Number += 1
+
+
+Num = 100
+while Num >= 2:
+    print(Num)
+    Num -= 2
+
+counter = 1
+Number = int(input("Enter number:"))
+multiply = 1
+
+while counter < 10:
+    multiply = counter * Number
+    print(counter, "X", Number, "=", multiply)
+    counter += 1
+
